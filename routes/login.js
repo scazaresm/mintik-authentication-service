a5e827d419894509dea7febdf37a417ae1466661
Sergio Cazares <scazares.dev@gmail.com>
2024-04-10 23:18:47 -0600

    Bug fix in login handler

--- routes/login.js
+++ routes/login.js
@@ -63,6 +63,7 @@ router.post('/', async (req, res) => {
         displayName: user.displayName,
         fullName: `${user.firstName} ${user.lastName}`,
         hasInitialPassword: user.hasInitialPassword,
+        enabled: user.enabled,
       },
       token,
     });
